40a01390c9244dd63788c8fe2d04f2df645faf88
unknown <business@martinaey.nl>
2024-06-15 23:04:28 +0200

    Added and updated icon stories.

--- dev/apps/storybook/stories/icon.stories.ts
+++ dev/apps/storybook/stories/icon.stories.ts
@@ -1,9 +1,8 @@
-import type {Args, Meta, StoryObj} from '@storybook/web-components';
+import type {Meta, StoryObj} from '@storybook/web-components';
 import "@martinaeynl/ui-components-icon";
 import {getWcStorybookHelpers} from "wc-storybook-helpers";
 import {UiSize, UiVariant} from "@martinaeynl/ui-component-models";
 import {html} from "lit";
-import {map} from "lit/directives/map.js";
 
 const helpers = getWcStorybookHelpers("ui-icon");
 
@@ -33,10 +32,22 @@ const meta: Meta = {
 type Story = StoryObj;
 
 export const Primary: Story = {
-    render: (args) => helpers.template(args),
-    args: {
-        title: "Button"
-    },
+    render: (args) => html`
+        <div style="display: flex; gap: 12px;">
+            ${helpers.template(args[0])}
+            ${helpers.template(args[1])}
+            ${helpers.template(args[2])}
+            ${helpers.template(args[3])}
+            ${helpers.template(args[4])}
+        </div>
+    `,
+    args: [
+        { title: "check-circle", icon: "check-circle", size: UiSize.XLARGE },
+        { title: "house-fill", icon: "house-fill", size: UiSize.XLARGE },
+        { title: "chevron-right", icon: "chevron-right", size: UiSize.XLARGE },
+        { title: "mic-fill", icon: "mic-fill", size: UiSize.XLARGE },
+        { title: "bar-chart-fill", icon: "bar-chart-fill", size: UiSize.XLARGE }
+    ],
 };
 
 export const VariantExample: Story = {
@@ -72,6 +83,27 @@ export const SizeExample: Story = {
         {title: "Large", icon: "check-circle", size: UiSize.LARGE},
         {title: "Medium", icon: "check-circle", size: UiSize.MEDIUM},
         {title: "Small", icon: "check-circle", size: UiSize.SMALL}
+    ]
+}
+
+export const SetExample: Story = {
+    render: (args) => html`
+        <div style="display: flex; gap: 12px;">
+            ${helpers.template(args[0])}
+            ${helpers.template(args[1])}
+            ${helpers.template(args[2])}
+            ${helpers.template(args[3])}
+            ${helpers.template(args[4])}
+            ${helpers.template(args[5])}
+        </div>
+    `,
+    args: [
+        {title: "bi", set: "bi", icon: "house", size: UiSize.XLARGE},
+        {title: "mdi", set: "mdi", icon: "house-outline", size: UiSize.XLARGE},
+        {title: "ant-design", set: "ant-design", icon: "home-outlined", size: UiSize.XLARGE},
+        {title: "fa6-solid", set: "fa6-solid", icon: "house", size: UiSize.XLARGE},
+        {title: "fluent", set: "fluent", icon: "home-48-regular", size: UiSize.XLARGE},
+        {title: "radix-icons", set: "radix-icons", icon: "home", size: UiSize.XLARGE},
     ]
 }
 
